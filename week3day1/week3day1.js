2092f6d14d03a92350fe49d81d5da47ffc494eeb
Hp <phoenix_vgg@mail.ru>
2021-11-09 21:07:30 +0300

    week3day1 work

---
+++ week3day1/week3day1.js
@@ -0,0 +1,72 @@
+// let a="0"
+//
+// console.log (a)
+
+// let age = prompt("сколько тебе лет", 100);
+// alert(`тебе ${age} лет`);
+
+// let num=3
+// const str = ''
+// let boolean = true
+//
+// console.log(!num)
+// console.log(!str)
+// console.log(!boolean)
+// задание4
+// let num = prompt(22)
+// alert(prompt(22))
+
+// задание5
+//
+// var a,b:integer;
+// begin
+// read(a,b);
+// write(a mod b);
+// end.
+
+
+// задание6
+// let a = 1, b = 1;
+//
+// let c = ++a; // ?
+// let d = b++; // ?
+//
+// let a = 1, b = 1;
+//
+// alert( ++a ); // 2, префиксная форма возвращает новое значение
+// alert( b++ ); // 1, постфиксная форма возвращает старое значение
+//
+// alert( a ); // 2, значение увеличено один раз
+// alert( b ); // 2, значение увеличено один раз
+
+
+// задание8
+// let num = 1=='1'
+// let num = 1==='1'
+// let boolean = 0 == false
+// let boolean = 0===false
+
+// // задание9
+// let num= 11
+// let str = "hello"
+// let num1= {a:1}
+// let boolean= null
+// let bool1 = false
+// console.log(typeof(num))str, num1, boolean, bool1));
+
+// задание10
+// let A=201
+// if (A % 2) alert(`A 'нечётное'); alert (`A чётное`)
+
+// задание11
+// const hasSpecialChars = (str) =>;
+//
+// const isStrongPassword = (password) => {
+//     const length = password.length;
+//         return (length > 8 && length < 20) || hasSpecialChars(password);
+//     alert ('Вы успешно вошли в систему')
+// };
+
+// задание12
+const a='30'
+alert(!!'a')
