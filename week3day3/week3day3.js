c4d6ffc585fbf279bff6f038488a30b2b92ef528
Hp <phoenix_vgg@mail.ru>
2021-11-17 20:43:12 +0300

    week3day3 work

---
+++ week3day3/week3day3.js
@@ -0,0 +1,72 @@
+// const factorial = (a) => {
+//     let result  = 1;
+//
+//     for (let counter = 1; counter <= a; counter++) {
+//         result *= counter;
+//     }
+//
+//     return result;
+// }
+
+
+//задание1
+// let a
+// for (let a=0; a<11; a++) {
+//     console.log (a)
+// }
+
+// let a = 1;
+//     do {
+//         console.log (a);
+//         a++;
+//     } while (a < 11); { //где то тут ошибка обьясните
+//     }
+
+// let a = 1
+// while (a < 11) {
+//     console.log (a)
+//     a++;
+// }
+
+// //задание2
+// let word = prompt('Введите слово для выхода из цикла');
+//     while (word != 'Остановить') ;
+//     alert('Вы вышли из цикла');
+
+
+//задание3
+// for (let i = 0; i < 20; i++) {
+//     if (i % 2 !== 0)
+//     {console.log(i)};
+// }
+//еще вариант
+// for (let i=0; i < 20; i++) {
+//     if (i% 2 ===1) {
+//         console.log(i) {
+//             else {
+//                 console.log(i)
+//             }
+//         }
+//     }
+// }
+//задание5
+// let a = 1, b = 3;
+// [a, b] = [b, a];
+// console.log(`${a} ${b}`);
+
+
+//задание6
+// let str = 143.2343.toFixed(2);
+// let num = num(str);
+
+//задание7
+// let num = Math.floor (42.2654*10)/10;
+// console.log(num)
+
+
+//задание8
+
+
+// 'I learn JavaScript'.indexOf('JavaScript');
+
+
