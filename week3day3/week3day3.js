f3ff01c38897cb721c4307b53a5b68ed19e3c6ea
Hp <phoenix_vgg@mail.ru>
2021-11-30 20:53:17 +0300

    Week3day3 1 часть сделал всю,на 2слишком много времени надо

--- week3day3/week3day3.js
+++ week3day3/week3day3.js
@@ -54,19 +54,77 @@
 // [a, b] = [b, a];
 // console.log(`${a} ${b}`);
 
-
 //задание6
-// let str = 143.2343.toFixed(2);
-// let num = num(str);
-
+// let numString = 143.2343.toFixed(2);
+// alert(numString)
 //задание7
-// let num = Math.floor (42.2654*10)/10;
-// console.log(num)
-
-
+// let num = 42.2654.toFixed(1);
+// alert(num)
 //задание8
-
-
+//не знаю как сделать
 // 'I learn JavaScript'.indexOf('JavaScript');
 
+//задание9
+// const generateSumFinder = () => {
+//     const sum = (a, b) => a + b;
+//     return sum = ;
+// };
+// const sum = generateSumFinder();
+// sum (1,5)
+// //еще вариант
+// let numSum = function (){
+//     let count = 0;
+//     return function (){
+//         count ++;
+//         return count;
+//     };
+// }();
+
+//задание10
+// let squarePow = (n, callbackFn) => {
+//     return callbackFn(n*n);
+// }
+// squarePow(2, (x) => {console.log(x*x);});
+
+
+//часть2!!!!
+
+//заданиние1
+
+// let obj = {a:'a', b:'b', c:'c'};
+//
+// console.log (a)
+//
+// let a = 10
+
+//задание2
+// let obj = { a: 2, b: 'Coding' };
+// let copyObj = Object.assign({}, obj);
+//
+// //вариант Spread-оператора
+// let newObj = { a: 2, b: 'Coding' },
+//     dablObj = [...newObj];
+
+//задание3
+// const obj = { a: 4, b: 5 }
+// ;
+// const obj1 = {b: 5 }
+// console.log(obj1)
+//
+// let user1 = {a: 4};
+// delete user1['a'];
+// user1.b = 5;
+// console.log(user1)
+
+//задание4
+// let user = new Object(
+//     name = 'Иван',
+//     lastName = 'Иванов',
+//     salary = 10
+// )
+//
+// for (key in obj) {
+//     console.log(`${key} = ${obj[key]}`);
+
+}
 
